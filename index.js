c0a5920e8f03a6263bd9285aafbc793c94de987a
JoelNeva <joel.nevala@gmail.com>
2025-11-11 08:37:38 +0200

    Add puhelinluettelon backend step6 assignment

--- index.js
+++ index.js
@@ -52,6 +52,25 @@ app.get('/api/info', (request, response) => {
 app.post('/api/persons', (request, response) => {
     console.log(request.headers)
     const body = request.body
+
+    if(!body.name){
+        return response.status(400).json({
+            error: 'name missing'
+        })
+    }
+
+    if(!body.number){
+        return response.status(400).json({
+            error: 'number missing'
+        })
+    }
+
+    if(persons.find(person => person.name === body.name)){
+        return response.status(400).json({
+            error: 'name must be unique'
+        })
+    }
+
     const person = {
         name: body.name,
         number: body.number,
